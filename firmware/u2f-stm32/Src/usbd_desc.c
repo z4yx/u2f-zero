83ad33ca2ee173a37afd48894ab4a0ae89e27b59
ZhangYuxiang <zz593141477@gmail.com>
2017-03-17 00:12:46 +0800

    USB VID/PID update

--- firmware/u2f-stm32/Src/usbd_desc.c
+++ firmware/u2f-stm32/Src/usbd_desc.c
@@ -66,11 +66,11 @@
 /** @defgroup USBD_DESC_Private_Defines
   * @{
   */ 
-#define USBD_VID     1155
+#define USBD_VID     4292
 #define USBD_LANGID_STRING     1033
 #define USBD_MANUFACTURER_STRING     "STMicroelectronics"
-#define USBD_PID_FS     22352
-#define USBD_PRODUCT_STRING_FS     "STM32 Custom Human interface"
+#define USBD_PID_FS     35535
+#define USBD_PRODUCT_STRING_FS     "U2F-TypeC"
 #define USBD_SERIALNUMBER_STRING_FS     "00000000001A"
 #define USBD_CONFIGURATION_STRING_FS     "Custom HID Config"
 #define USBD_INTERFACE_STRING_FS     "Custom HID Interface"
